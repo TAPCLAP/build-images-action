94c9d4bc710d55444d9a4bc46395a750596274d1
Dmitry Sergeev <identw@gmail.com>
2025-09-09 20:25:00 +0400

    fix problem with template not strings

--- js-action/src/lib.js
+++ js-action/src/lib.js
@@ -87,6 +87,10 @@ export function template(str) {
   const context         = github.context;
   const shortCommit     = context.sha.slice(0, 10);
   const refName         = normalizeRefName(context.ref);
+  
+  if (typeof str !== "string") {
+    str = String(str); 
+  }
 
   str = str.replaceAll("{{ commit }}", shortCommit);
   str = str.replaceAll("{{ dateTime }}", getCurrentUtcTimestamp());
